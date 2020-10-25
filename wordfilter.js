6bf21cd43a308e46b34c129366adfdea9c0a0c15
RubikNube <roland@kiesendahl.de>
2020-10-25 17:18:46 +0100

    added json to specify filters

--- wordfilter.js
+++ wordfilter.js
@@ -1,15 +1,44 @@
 jQuery(function () {
     console.log("JQuery is loaded!");
-    let re = "\*innen";
 
-    $("*")
-        .contents()
-        .filter(function () {
-            return this.nodeType == 3 && this.textContent.includes(re);
-        })
-        .replaceWith(function () {
-            let tmp = this.textContent.replace(re, 'en');
-            console.log("Replaced " + this.textContent + " with " + tmp)
-            return tmp;
-        });
+    let data = {
+        "filters": [
+            {
+                "word": "Schüler*innen",
+                "replacement": "Schüler"
+            },
+            {
+                "word": "*innen",
+                "replacement": "en"
+            },
+            {
+                "word": ":innen",
+                "replacement": "en"
+            },
+            {
+                "word": "*e",
+                "replacement": ""
+            },
+            {
+                "word": "*in",
+                "replacement": ""
+            }
+        ]
+    };
+
+    for (let i = 0; i < data.filters.length; i++) {
+        let word = data.filters[i].word;
+        let replacement = data.filters[i].replacement;
+
+        $("*")
+            .contents()
+            .filter(function () {
+                return this.nodeType == 3 && this.textContent.includes(word);
+            })
+            .replaceWith(function () {
+                let tmp = this.textContent.replace(word, replacement);
+                console.log("Replaced '" + this.textContent + "' with '" + tmp + "'")
+                return tmp;
+            });
+    }
 })
