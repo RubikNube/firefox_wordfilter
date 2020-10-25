d8b1991657356484823fd9cd6cae87cd3b0862b3
RubikNube <roland@kiesendahl.de>
2020-10-25 16:22:11 +0100

    added missing files

--- wordfilter.js
+++ wordfilter.js
@@ -1,1 +1,15 @@
-document.body.style.border = "5px solid red";
+jQuery(function () {
+    console.log("JQuery is loaded!");
+    let re = "\*innen";
+
+    $("*")
+        .contents()
+        .filter(function () {
+            return this.nodeType == 3 && this.textContent.includes(re);
+        })
+        .replaceWith(function () {
+            let tmp = this.textContent.replace(re, 'en');
+            console.log("Replaced " + this.textContent + " with " + tmp)
+            return tmp;
+        });
+})
